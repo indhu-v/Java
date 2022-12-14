f8e08725bc8dbea5937d972e7d2fad191c685f42
indhu-v <113018637+indhu-v@users.noreply.github.com>
2022-12-14 08:51:50 +0530

    Add files via upload

---
+++ reversestr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class reversestr
+{
+public static void main(String[]args)
+{
+String str;
+char ch;
+Scanner s=new Scanner(System.in);
+System.out.println("enter a string:");
+str=s.nextLine();
+System.out.println("reverse of a string "+str+" is:");
+for(int j=str.length();j>0;--j)
+{
+System.out.print(str.charAt(j-1));
+}
+}
+}
